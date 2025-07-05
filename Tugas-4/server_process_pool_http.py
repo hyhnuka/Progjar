e801b7b7d9e6e37910f78801f6eaf662fb484ce7
hyhnuka <156576309+hyhnuka@users.noreply.github.com>
2025-07-05 15:54:26 +0700

    Add files via upload

---
+++ Tugas-4/server_process_pool_http.py
@@ -0,0 +1,88 @@
+from socket import *
+import socket
+import time
+import sys
+import logging
+import multiprocessing
+from concurrent.futures import ProcessPoolExecutor
+from http import HttpServer
+
+httpserver = HttpServer()
+
+#untuk menggunakan processpoolexecutor, karena tidak mendukung subclassing pada process,
+#maka class ProcessTheClient dirubah dulu menjadi function, tanpda memodifikasi behaviour didalamnya
+
+# ...existing code...
+def ProcessTheClient(connection, address):
+    rcv = b""
+    headers_ended = False
+    content_length = 0
+    while True:
+        try:
+            data = connection.recv(1024*1024)
+            if data:
+                rcv += data
+                if not headers_ended:
+                    if b"\r\n\r\n" in rcv:
+                        headers_ended = True
+                        header, rest = rcv.split(b"\r\n\r\n", 1)
+                        header_str = header.decode()
+                        for line in header_str.split("\r\n"):
+                            if line.lower().startswith("content-length:"):
+                                content_length = int(line.split(":", 1)[1].strip())
+                        body = rest
+                        # Jika body sudah lengkap
+                        while len(body) < content_length:
+                            more = connection.recv(1024*1024)
+                            if not more:
+                                break
+                            body += more
+                        full_request = header + b"\r\n\r\n" + body
+                        hasil = httpserver.proses(full_request.decode(errors='ignore'))
+                        hasil = hasil + "\r\n\r\n".encode()
+                        connection.sendall(hasil)
+                        connection.close()
+                        return
+                else:
+                    # Untuk request tanpa body
+                    hasil = httpserver.proses(rcv.decode(errors='ignore'))
+                    hasil = hasil + "\r\n\r\n".encode()
+                    connection.sendall(hasil)
+                    connection.close()
+                    return
+            else:
+                break
+        except OSError as e:
+            pass
+    connection.close()
+    return
+
+
+
+def Server():
+	the_clients = []
+	my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+	my_socket.bind(('0.0.0.0', 8889))
+	my_socket.listen(1)
+
+	with ProcessPoolExecutor(20) as executor:
+		while True:
+				connection, client_address = my_socket.accept()
+				#logging.warning("connection from {}".format(client_address))
+				p = executor.submit(ProcessTheClient, connection, client_address)
+				the_clients.append(p)
+				#menampilkan jumlah process yang sedang aktif
+				jumlah = ['x' for i in the_clients if i.running()==True]
+				print(jumlah)
+
+
+
+
+
+def main():
+	Server()
+
+if __name__=="__main__":
+	main()
